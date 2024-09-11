890e5c3dc21a36bd90cccde4d4e231954e0370ad
lianasabino <lianasabino@hotmail.com>
2024-09-11 11:57:04 -0300

    create first route called 'createGoal'

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+//Conexão com o banco de dados:
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+import { env } from "../env";
+
+export const client = postgres(env.DATABASE_URL);
+export const db = drizzle(client, { schema, logger: true });
